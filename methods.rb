dbc7169054108fef3d7b631f3027ae819b7030e8
edd-ie <ogendoeddie@gmail.com>
2023-05-15 18:49:57 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,19 @@
 # Your code here!
+def greet_programme
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(num)
+    if(num.class == "String")
+        return nil
+    else
+        num/2
+end
